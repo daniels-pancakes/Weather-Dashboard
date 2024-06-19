a922f1495fd510c154799448dc479d7f7c5a6d65
Daniel D <ddunnemann@gmail.com>
2024-06-19 16:48:03 -0500

    Successfully connected to the Open Weather 5 day forecast. Able to console log the 5 day forecast. Now to make sense of this data its given us back.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,45 @@
-const APIkey = "&=cbfda5c198563b28c04126c28f33c8f6";
+const APIkey = "&appid=cbfda5c198563b28c04126c28f33c8f6";
 let searchForm = document.getElementById("search-form");
 let entry = document.getElementById("search-value");
-let citylat;
-let citylong;
+let processedEntry;
+let cityLat;
+let cityLon;
+let requestURL;
+let forecastURL;
 
 function handleFormSubmit(event) {
     event.preventDefault();
-    console.log(entry.value.trim().replaceAll(" ", "_"));
+    cityq = entry.value.trim().replaceAll(" ", "_");
+
+    requestURL = "https://api.openweathermap.org/geo/1.0/direct?q=" + cityq + "&limit=1" + APIkey;
+    fetch(requestURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            console.log(data[0].lat);
+            console.log(data[0].lon);
+            cityLat = data[0].lat;
+            cityLon = data[0].lon;
+            forecastURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + cityLat + "&lon=" + cityLon + "&units=imperial" + APIkey;
+            console.log(forecastURL);
+            fetch(forecastURL)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    console.log(data);
+                })
+
+        });
+
     entry.value = "";
 };
 
 searchForm.addEventListener("submit", handleFormSubmit);
 
-let requestURL;
+
 
 // https://api.openweathermap.org/data/2.5/forecast?
 //lat= &lon=
