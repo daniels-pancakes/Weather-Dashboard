b4dececfd0c73825d5956a640f47ad181f9d1a45
Daniel D <ddunnemann@gmail.com>
2024-06-19 16:21:49 -0500

    Accounting for the fact the URL is going to require city names be separated by underscores.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,23 @@
-const APIkey = "cbfda5c198563b28c04126c28f33c8f6";
-const searchForm = document.getElementById("search-form");
-let whatever = document.getElementById("search-value")
-
+const APIkey = "&=cbfda5c198563b28c04126c28f33c8f6";
+let searchForm = document.getElementById("search-form");
+let entry = document.getElementById("search-value");
+let citylat;
+let citylong;
 
 function handleFormSubmit(event) {
     event.preventDefault();
-    console.log(whatever.value);
+    console.log(entry.value.trim().replaceAll(" ", "_"));
+    entry.value = "";
 };
 
 searchForm.addEventListener("submit", handleFormSubmit);
+
+let requestURL;
+
+// https://api.openweathermap.org/data/2.5/forecast?
+//lat= &lon=
+
+// &units=imperial
+// APIkey
+
+//https://api.openweathermap.org/geo/1.0/direct?q=   &limit=1" + APIkey
